700221d6a001818954bac4bb170b5175b3e549e8
TakisuGui <3193676710@qq.com>
2026-05-12 23:14:56 +0800

    cpp/算法/动态规划/dp_树型dp_找到二叉树中的最大搜索二叉子树.class

--- cpp/算法/动态规划/dp_树型dp_找到二叉树中的最大搜索二叉子树.java
+++ cpp/算法/动态规划/dp_树型dp_找到二叉树中的最大搜索二叉子树.java
@@ -1,71 +1,53 @@
+import java.util.ArrayList;
+
 public class dp_树型dp_找到二叉树中的最大搜索二叉子树 {
 
-    public static class TreeNode {
-		public int val;
-		public TreeNode left;
-		public TreeNode right;
+public static int longestPath(int[] parent, String str) {
+		int n = parent.length;
+		char[] s = str.toCharArray();
+		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			graph.add(new ArrayList<>());
+		}
+		for (int i = 1; i < n; i++) {
+			graph.get(parent[i]).add(i);
+		}
+		return f(s, graph, 0).maxPath;
 	}
 
+	public static class Info {
+		public int maxPathFromHead; // 一定要从头节点出发的情况下，相邻字符不等的最长路径长度
+		public int maxPath; // 整棵树上，相邻字符不等的最长路径长度
 
+		public Info(int a, int b) {
+			maxPathFromHead = a;
+			maxPath = b;
+		}
+	}
 
+	public static Info f(char[] s, ArrayList<ArrayList<Integer>> graph, int u) {
+		if (graph.get(u).isEmpty()) {
+			// u节点是叶
+			return new Info(1, 1);
+		}
+		int max1 = 0; // 下方最长链
+		int max2 = 0; // 下方次长链
+		int maxPath = 1;
+		for (int v : graph.get(u)) {
+			Info nextInfo = f(s, graph, v);
+			maxPath = Math.max(maxPath, nextInfo.maxPath);
+			if (s[u] != s[v]) {
+				if (nextInfo.maxPathFromHead > max1) {
+					max2 = max1;
+					max1 = nextInfo.maxPathFromHead;
+				} else if (nextInfo.maxPathFromHead > max2) {
+					max2 = nextInfo.maxPathFromHead;
+				}
+			}
+		}
+		int maxPathFromHead = max1 + 1;
+		maxPath = Math.max(maxPath, max1 + max2 + 1);
+		return new Info(maxPathFromHead, maxPath);
+	}
 
-    public static int largestBSTSubtree(TreeNode root)
-    {
-        return f(root).maxBstSize;
-    }
-
-    public static class Info
-    {
-        public long max;
-		public long min;
-		public boolean isBst;
-		public int maxBstSize;
-
-
-        public Info(long a, long b, boolean c, int d)
-        {
-			max=a;
-			min=b;
-			isBst=c;
-			maxBstSize=d;
-        }
-    }
-
-
-    public static Info f(TreeNode x)
-    {
-        if(x==null) return new Info(Long.MIN_VALUE,Long.MAX_VALUE,true,0);
-
-        Info infol=f(x.left);
-        Info infor=f(x.right);
-
-        long max=Math.max(x.val,Math.max(infol.max,infor.max));
-        long min=Math.min(x.val,Math.min(infol.min,infor.min));
-
-        boolean isBst=(infol.isBst&&infor.isBst)&& (infol.max<x.val && x.val<infor.min);
-
-        int maxBSTSize;
-        if(isBst) maxBSTSize=infol.maxBstSize+infor.maxBstSize+1;
-        else maxBSTSize=Math.max(infol.maxBstSize,infor.maxBstSize);
-
-        return new Info(max,min,isBst,maxBSTSize);
-    }
-
-    public static void main(String[] args) {
-        TreeNode root = new TreeNode();
-        root.val = 10;
-        root.left = new TreeNode();
-        root.left.val = 5;
-        root.right = new TreeNode();
-        root.right.val = 15;
-        root.left.left = new TreeNode();
-        root.left.left.val = 1;
-        root.left.right = new TreeNode();
-        root.left.right.val = 7;
-        root.right.right = new TreeNode();
-        root.right.right.val = 20;
-
-        int result = largestBSTSubtree(root);
-        System.out.println("最大二叉搜索树的大小: " + result);
-    }
 }
